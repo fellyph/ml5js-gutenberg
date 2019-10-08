9160fcad6a96b636b4ca985c41f95f9ce9b7e708
fellyph <fellyph.cintra@gmail.com>
2019-10-08 22:59:17 +0100

    adding savemodel exaample

--- build/frontend.js
+++ build/frontend.js
@@ -1,21 +1,17 @@
-let mobilenet;
+let featureExtrator;
 let classifier;
 let video;
+let controlsContainer;
+let containerBlock;
 let label = 'wc thessaloniki';
 let product1;
-let Product2;
+let product2;
+let product3;
 let trainButton;
+let saveButton;
 let scale = 0;
 let imageWidth = 320;
 let imageHeight = 270;
-
-function modelReady() {
-  console.log('Model is ready !!!');
-}
-
-function videoReady() {
-  console.log('Video is ready !!!');
-}
 
 function whileTraining(loss) {
   if (loss === null) {
@@ -26,11 +22,11 @@ function whileTraining(loss) {
   }
 }
 
-
 function gotResults(error, result) {
   if (error) {
     console.error(error);
   } else {
+    console.log(result)
     label = `${result[0].label}`;
     scale = result[0].confidence;
     classifier.classify(gotResults);
@@ -38,40 +34,60 @@ function gotResults(error, result) {
 }
 
 function setup() {
-  let containerBlock = document.querySelector('.product-classifier');
+  containerBlock = document.querySelector('.product-classifier');
   let myCanvas = createCanvas(imageWidth, imageHeight);
+
   myCanvas.parent(containerBlock);
   video = createCapture(VIDEO);
   video.hide();
   background(0);
 
-  mobilenet = ml5.featureExtractor('MobileNet', modelReady);
-  classifier = mobilenet.classification(video, videoReady);
+  featureExtrator = ml5.featureExtractor('MobileNet');
+  classifier = featureExtrator.classification(video, { numLabels: 3 }, () => {
+    console.log('classifier is ready!')
+  });
+  setupButton();
+}
+
+function setupButton() {
+  controlsContainer = createDiv();
+  controlsContainer.parent(containerBlock);
 
   product1 = createButton('Product 1');
-  product1.parent(containerBlock);
+  product1.parent(controlsContainer);
   product1.mousePressed(function() {
     classifier.addImage('Product 1');
   });
 
-  Product2 = createButton('Product 2');
-  Product2.parent(containerBlock);
-  Product2.mousePressed(function() {
+  product2 = createButton('Product 2');
+  product2.parent(controlsContainer);
+  product2.mousePressed(function() {
     classifier.addImage('Product 2');
   });
 
+  product3 = createButton('Product 3');
+  product3.parent(controlsContainer);
+  product3.mousePressed(function() {
+    classifier.addImage('Product 3');
+  });
+
   trainButton = createButton('train');
-  trainButton.parent(containerBlock);
+  trainButton.parent(controlsContainer);
   trainButton.mousePressed(function() {
     classifier.train(whileTraining);
+  });
+
+  saveButton = createButton('Saves');
+  saveButton.parent(controlsContainer);
+  saveButton.mousePressed(function() {
+    classifier.save();
   });
 }
 
 function draw() {
   background(0);
-  image(video, 0, 0, imageHeight, imageHeight - 30);
+  image(video, 0, 0, imageWidth, imageHeight - 30);
   fill(255);
   textSize(16);
   text(label, 10, height - 10);
-  rect()
 }
